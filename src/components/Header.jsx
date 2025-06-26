77a897c1ce457368146aa3e92f5de3d49fcd3b9f
Eudantas <eudantas.alves@gmail.com>
2025-06-25 21:41:46 -0300

    navbar com menu hamburger

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,28 @@
-
 // src/components/Header.jsx
 import React from "react";
 import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
       <div className="container">
         <Link className="navbar-brand" to="/">Estúdio Tattoo</Link>
-        <div className="collapse navbar-collapse">
+
+        {/* Botão hamburguer (visível em telas menores) */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Menu que colapsa (hamburguer) */}
+        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <Link className="nav-link" to="/">Home</Link>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-light bg-light px-3">
+      <Link to="/" className="navbar-brand">
+        Tattoo Art
+      </Link>
+
+      {/* Botão hamburguer (visível em telas pequenas) */}
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      {/* Links que colapsam no botão hamburguer */}
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/portfolio" className="nav-link">
+              Portfólio
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/cadastro" className="nav-link">
+              Cadastro
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
